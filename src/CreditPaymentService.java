1bd5d237cadad5bf423c00845094e1d513b352bb
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-03 20:29:31 +0300

    Upgraded CreditCalculator

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,8 @@
 public class CreditPaymentService {
     public double calculete(double credit, double interestRate, double creditMonths) {
-        double yearPercent = interestRate / 100 / 12;
+        double yearMonth = 12;
+        double percent = 100;
+        double yearPercent = interestRate / percent / yearMonth;
         double monthPercent = 1 + yearPercent;
         double monthPay = credit * (yearPercent * (Math.pow(monthPercent, creditMonths) / (Math.pow(monthPercent, creditMonths) - 1)));
         return monthPay;
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService calculator = new CreditPaymentService();
+        String monthPayString = "Ежемесячный платеж ";
 
         double user1 = calculator.calculete(1_000_000, 9.99, 12);
-        System.out.println("Ежемесячный платеж " + user1);
+        System.out.println(monthPayString + user1);
 
         double user2 = calculator.calculete(300_000, 9, 6);
-        System.out.println("Ежемесячный платеж " + user2);
+        System.out.println(monthPayString + user2);
 
         double user3 = calculator.calculete(4_087_000, 7.5, 24);
-        System.out.println("Ежемесячный платеж " + user3);
+        System.out.println(monthPayString + user3);
 
         double user4 = calculator.calculete(11_000_000, 6.5, 240);
-        System.out.println("Ежемесячный платеж " + user4);
+        System.out.println(monthPayString + user4);
 
     }
 }
