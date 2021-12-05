3f7d05efa846740fca3f93263f57e8d7cc13656e
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-06 00:40:47 +0300

    Upgreaded CreditCalculator

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,11 @@ public class Main {
         double user1 = calculator.calculete(1_000_000, 9.99, 12);
         System.out.println(monthPayString + user1);
 
-        double user2 = calculator.calculete(300_000, 9, 6);
+        double user2 = calculator.calculete(1_000_000, 9.99, 24);
         System.out.println(monthPayString + user2);
 
-        double user3 = calculator.calculete(4_087_000, 7.5, 24);
+        double user3 = calculator.calculete(1_000_000, 9.99, 36);
         System.out.println(monthPayString + user3);
-
-        double user4 = calculator.calculete(11_000_000, 6.5, 240);
-        System.out.println(monthPayString + user4);
 
     }
 }
